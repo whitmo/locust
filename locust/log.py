b674caeb8070c7ceda4e8837572199292932eedb
whitmo <d.w.morriss@gmail.com>
2012-11-19 16:02:55 -0600

    remove uneeded wrappers for stdout (makes pdb work again)

--- locust/log.py
+++ locust/log.py
@@ -1,5 +1,5 @@
 import logging
-import sys
+
 
 def setup_logging(loglevel, logfile):
     numeric_level = getattr(logging, loglevel.upper(), None)
@@ -8,27 +8,8 @@ def setup_logging(loglevel, logfile):
     
     log_format = "[%(asctime)s] %(levelname)s/%(name)s: %(message)s"
     logging.basicConfig(level=numeric_level, filename=logfile, format=log_format)
+
     
-    sys.stderr = StdErrWrapper()
-    sys.stdout = StdOutWrapper()
-
-stdout_logger = logging.getLogger("stdout")
-stderr_logger = logging.getLogger("stderr")
-
-class StdOutWrapper(object):
-    """
-    Wrapper for stdout
-    """
-    def write(self, s):
-        stdout_logger.info(s.strip())
-
-class StdErrWrapper(object):
-    """
-    Wrapper for stderr
-    """
-    def write(self, s):
-        stderr_logger.error(s.strip())
-
 # set up logger for the statistics tables
 console_logger = logging.getLogger("console_logger")
 # create console handler
